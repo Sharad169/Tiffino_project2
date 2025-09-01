19fc9962ecb7793bf1568e52e13bd95f6d7ab874
Sharad169 <sharadmohite7020@gmail.com>
2025-09-01 17:00:21 +0530

    Added Routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
