f33623fd588375a3fdbcab1a79ebf8e8974e910b
Karanthorat46 <karanthorat033@gmail.com>
2025-09-03 14:36:53 +0530

    home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { UserSignInComponent } from './registration/user-sign-in/user-sign-in.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     {path : "home", component : HomeComponent},
 
     //Registration
