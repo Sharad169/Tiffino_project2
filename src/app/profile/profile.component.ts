8ea5c72858c663f6677b7c75e62cc92f235831b0
Sharad169 <sharadmohite7020@gmail.com>
2025-10-09 19:34:00 +0530

    profile page binding complete

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../service/user.service';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
@@ -13,11 +15,15 @@ import { ActivatedRoute } from '@angular/router';
 export class ProfileComponent implements OnInit {
   userEmail: string = 'aditya@gmail.com';
     userData: any;
+    selectedTabName : string = "Account Settings";
+   sideBarItems = ['Account Settings','Order History','Payment Method','Addresses','Help Center','Promocodes & Offers','Meal Plan Subscription','Invite Friends','Privacy','Logout','Delete Account'];
+
 
   constructor(private userService: UserService, private route: ActivatedRoute) {}
 
    ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id'); // URL से id लेना
+     // For testing purpose only, remove this line in production
+    const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.getUserDetails(+id);
     }
@@ -26,14 +32,33 @@ export class ProfileComponent implements OnInit {
   getUserDetails(id: number) {
     this.userService.getUserById(id).subscribe({
       next: (data) => {
-        console.log('✅ User data:', data);
+        console.log('User data:', data);
         this.userData = data;
+        sessionStorage.setItem('userName', this.userData.name);
       },
       error: (err) => {
-        console.error('❌ Error fetching user:', err);
+        console.error('Error fetching user:', err);
       }
     });
   }
+
+
+  bindFiled(){
+    this.userData = {
+  name: this.userData.name,
+  email: this.userData.email,
+  phone: this.userData.phone,
+  dateOfBirth: this.userData.dateOfBirth
+};
+  }
+
+  selectTab(tabName: string) {
+    console.log('Selected Tab:', tabName);
+    this.selectedTabName = tabName;
+    
+  }
+
+
 
 
 }
