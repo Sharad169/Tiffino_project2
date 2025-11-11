112c5d750b5cbdcd12ff9058594a9f7454b53e8d
Sharad169 <sharadmohite7020@gmail.com>
2025-11-11 12:52:09 +0530

    manager registration page completed

--- src/app/superadmincloudkitchen/superadmincloudkitchen.component.ts
+++ src/app/superadmincloudkitchen/superadmincloudkitchen.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [SuperadminSidebarComponent,CommonModule,ReactiveFormsModule],
   templateUrl: './superadmincloudkitchen.component.html',
-styleUrls: ['./superadmincloudkitchen.component.css']
+  styleUrls: ['./superadmincloudkitchen.component.css']
 })
 export class SuperadmincloudkitchenComponent {
    addressForm!: FormGroup;
