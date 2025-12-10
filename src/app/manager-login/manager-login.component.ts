3fa7cfa04f2abcac53efd3b0fb615e024e08a743
Aadi070400 <salunkheaaditya8@gmail.com>
2025-12-10 11:54:32 +0530

    chages done manager-login

--- src/app/manager-login/manager-login.component.ts
+++ src/app/manager-login/manager-login.component.ts
@@ -30,7 +30,6 @@ export class ManagerLoginComponent {
       .login(managerCode, this.password, this.tempPass)
       .subscribe({
         next: (res) => {
-          debugger;
           console.log('Login success:', res);
           alert('Login successful');
         },
